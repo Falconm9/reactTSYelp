d1cb8d0da601f8ae5c8c9d66ec8192e4f76b979b
unknown <joselfalconml@hotmail.com>
2023-09-03 12:57:01 -0600

    changes on lists

--- src/components/lists/restaurantsList.tsx
+++ src/components/lists/restaurantsList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './lists.css'
 import { Restaurants } from '@/utils/types';
 import Image from 'next/image';
@@ -12,33 +12,87 @@ interface RestaurantsListProps {
 }
 
 const RestaurantsLists:React.FC<RestaurantsListProps> = ({ infoList, loadingFilter }) => {
+  const [isScreenSmall, setIsScreenSmall] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsScreenSmall(window.innerWidth <= 500); 
+    };
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   const handleView = (url: string) => {
-    window.open(url, '_blank');
+    window.open(url, '_blank')?.focus();
+  }
+
+  const MultiList = () => {
+    return(
+      <>
+        { infoList.map((item) => (
+            <div key={item.id} className="card">
+              <Image 
+                src={item.image_url}
+                width={140}
+                height={140}
+                alt=''
+              />
+              <div className='title'>
+                <h4>{item.name}</h4>
+              </div>
+              <div className='info'>
+                <StarRating rating={item.rating} />
+                <div className='priceInfo'>
+                  <span className='priceText'>{item.price ? item.price : 'No price info'}</span>
+                </div>
+              </div>
+              <button className="button-View" onClick={() => handleView(item.url)}>View</button>
+            </div>
+          ))
+        }
+      </>
+    )
+  }
+
+  const SimpleList = () => {
+    return (
+      <>
+      { infoList.map((item) => (
+          <div key={item.id} className="card-Simple">
+            <Image 
+              src={item.image_url}
+              width={100}
+              height={100}
+              alt=''
+            />
+            <div className='infoCard'>
+              <div className='title'>
+                <span className='titleText'>{item.name}</span>
+              </div>
+              <div className='info-SimpleCard'>
+                <StarRating rating={item.rating} />
+                <div className='priceInfo'>
+                  <span className='priceText'>{item.price ? item.price : 'No price info'}</span>
+                </div>
+              </div>
+              <button className="button-View-SimpleCard" onClick={() => handleView(item.url)}>View</button>  
+            </div>
+          </div>
+        ))
+      }
+      </>
+    )
   }
 
   return (
     <div className="cards-list">
       {loadingFilter && <LoadingDots />}
-      {!loadingFilter && infoList.map((item) => (
-        <div key={item.id} className="card">
-          <Image 
-            src={item.image_url}
-            width={140}
-            height={140}
-            alt=''
-          />
-          <div className='title'>
-            <h4>{item.name}</h4>
-          </div>
-          <div className='info'>
-            <StarRating rating={item.rating} />
-            <div className='priceInfo'>
-              <span className='priceText'>{item.price ? item.price : 'No price info'}</span>
-            </div>
-          </div>
-          <button className="button-View" onClick={() => handleView(item.url)}>View</button>
-        </div>
-      ))}
+      {!loadingFilter && !isScreenSmall && <MultiList />}
+      {!loadingFilter && isScreenSmall && <SimpleList />}
     </div>
   )
 }
